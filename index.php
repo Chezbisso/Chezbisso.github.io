3eb03772cd4b86ab931bb6fab0e6e46275ac1ef4
hkfmz <hyperpro200497@gmail.com>
2021-08-03 15:23:47 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="robots" content="noindex">
+		<title>En construction</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Permanent+Marker">
+		<style>
+			body { font-family: 'Times New Roman'; text-align: center;}
+			h1 { color: #FFCC99; text-shadow: 3px 3px 0 #f4f4f4; margin-top: calc(50vh - 150px); font-size: 3em; }
+			img { width: 300px; }
+		</style>
+	</head>
+
+	<body>
+		<h1>Welcome to chezbisso !&#8239;!</h1>
+		<p>Page en création revenez plutard ;-)</p>
+		<a href="#"><img src="https://i.imgur.com/L68hII8.gif" /></a>
+	</body>
+</html>
